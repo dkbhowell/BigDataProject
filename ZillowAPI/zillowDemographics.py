e5811bf12c908e3c86e2fcb5160df26e421aef83
ormnv <romanova.olga.a@gmail.com>
2014-10-30 21:59:46 -0400

    adding paper

--- ZillowAPI/zillowDemographics.py
+++ ZillowAPI/zillowDemographics.py
@@ -6,21 +6,18 @@ import xml.etree.ElementTree as etree
 zips = file('zipcodes.txt', 'r')
 url = 'http://www.zillow.com/webservice/GetDemographics.htm?zws-id=X1-ZWz1b260i9k4jv_7j3qs&zip='
 outputName = 'output.txt'
-# os.remove(output.txt)
+os.remove(outputName)
 output = open(outputName, 'w')
 output.write('')
 
 for line in zips:
 	file = urllib2.urlopen(url + line)
-	tree = etree.parse(file)
-	notags = etree.tostring(tree, encoding='utf8', method='text')
-	print(notags)
-	# for line in file:
-	# 	#data = file.readline()
-	# 	print line
-	# 	output.write(line)
-	output.write(notags)
-	output.write("\n")
+	# for l in file:
+	data = file.read()
+	print data
+	output.write(data)
+	# output.write("\n")
+	# output.write("\n")
 file.close()
 output.close()
-neighborhoods.close()
+zips.close()
