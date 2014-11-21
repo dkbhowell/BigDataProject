02fe56244809efc4fa11c8c5b4f6488d9cc170f2
Martha Poole <marthaepoole@gmail.com>
2014-11-20 22:16:44 -0500

    most recent code update

--- NYCData/PricesMapper (1).java
+++ NYCData/PricesMapper (1).java
@@ -2,7 +2,6 @@
 import java.io.IOException;
 
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -13,39 +12,49 @@ extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 public void map(LongWritable key, Text value, Context context)
 throws IOException, InterruptedException {
 String line = value.toString();
-String[] field = line.split(",");
+String[] field = line.split("\\|");
+Double count =1.0;
 
 //make sure that it's a valid 
-if (field[0] !="1" || field[0] !="2" || field[0] !="3" || field[0] !="4" || field[0] !="5"){
+if (field.length !=21){
+	String leng = String.valueOf(field.length);
+	context.write(new Text("failed condition0"), new DoubleWritable(count));
+}
+else if (!field[0].contains("1") && !field[0].contains("2") && !field[0].contains("3") && !field[0].contains("4")&& !field[0].contains("5")){
+	context.write(new Text("failed condition1"), new DoubleWritable(count));
 	//do nothing
 }
 //get rid of non-residential properties
 else if (field[2].contains("05") || field[2].contains("06") || field[2].contains("21") || field[2].contains("22") || field[2].contains("27") || field[2].contains("29") || field[2].contains("30") || field[2].contains("31") || field[2].contains("32") || field[2].contains("33") || field[2].contains("35") || field[2].contains("36") || field[2].contains("37") || field[2].contains("38") || field[2].contains("39") || field[2].contains("40") || field[2].contains("41") || field[2].contains("43") || field[2].contains("44") || field[2].contains("46") || field[2].contains("47")){
-
+	context.write(new Text("failed condition2"), new DoubleWritable(count));
 }
 //make sure square feet is listed
 else if(field[15] == "0"){
-	
+	context.write(new Text("failed condition3"), new DoubleWritable(count));
 }
 //make sure price is listed
 else if (field[19].contains("$-") || field[19].contains("$0")){
-	
+	context.write(new Text("failed condition4"), new DoubleWritable(count));
 }
 else if (field[10] ==""){
-	
+	context.write(new Text("failed condition5"), new DoubleWritable(count));
+}
+else if (field[19]==""){
+	context.write(new Text("failed condition5"), new DoubleWritable(count));
 }
 else{
 	String sqFeet= field[15];
 	String price =field[19];
-	sqFeet.replaceAll("^[0-9]", "");
-	price.replaceAll("^[0-9]", "");
+	sqFeet =sqFeet.replaceAll("[$,]", "");
+	price =price.replaceAll("[$,]", "");
 	Double sqFeetInt=Double.parseDouble(sqFeet);
 	Double priceInt=Double.parseDouble(price);
 	Double pricePerFoot=priceInt/sqFeetInt;
 	String date = field[20];
-	String year = date.replace("[^/]*/[^/]*/", "");
-	String yearZip = field[10]+year;
+	String year = date.replaceAll("[^/]*/[^/]*/", "");
+	String yearZip = field[10]+ "|" +year;
 	context.write(new Text(yearZip), new DoubleWritable(pricePerFoot));
+	//context.write(new Text(price), new DoubleWritable(count));
 	
 }
 
--- NYCData/PricesReducer (1).java
+++ NYCData/PricesReducer (1).java
@@ -3,20 +3,22 @@
 import java.io.IOException;
 
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
+
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
 public class PricesReducer
-extends Reducer<Text, Text, Text, DoubleWritable> {
+extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+@Override
 public void reduce(Text key, Iterable<DoubleWritable> values,
 Context context)
 throws IOException, InterruptedException {
-	Double total = 1.0;
-	Double runningAvg=0.0;
-for (DoubleWritable value : values){
-	runningAvg = runningAvg*(total-1)/total + value.get()/total;
-	total = total +1;	
+Double newAvg = 0.0;
+Double total = 1.0;
+for (DoubleWritable value : values) {
+newAvg = newAvg*(total-1)/total + value.get()/total;
+total++;
 }
-context.write(key, new DoubleWritable(runningAvg));
+context.write(key, new DoubleWritable(newAvg));
 }
 }
