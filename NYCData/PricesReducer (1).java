52f3efa748387b699e8a3a6cc6d6ec739fd9ad7d
Martha Poole <marthaepoole@gmail.com>
2014-12-01 11:40:42 -0500

    adding updated code and hadoop output

--- NYCData/PricesReducer (1).java
+++ NYCData/PricesReducer (1).java
@@ -15,12 +15,25 @@ Context context)
 throws IOException, InterruptedException {
 Double count = 0.0;
 Double total = 1.0;
+Double countFail=0.0;
+
+
 for (DoubleWritable value : values) {
+	if (key.toString().contains("failed")){
+		countFail=countFail+1;
+	}
+	else{
 total=total+value.get();
 //newAvg = newAvg*(total-1)/total + value.get()/total;
 //total++;
 count++;
+	}
 }
+if (key.toString().contains("failed")){
+	context.write(key, new DoubleWritable(countFail));
+}
+else{
 context.write(key, new DoubleWritable(total/count));
+}
 }
 }
