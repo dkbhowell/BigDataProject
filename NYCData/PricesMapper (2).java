63464faa7c76f73dfe60d049f350cb3210d7508b
Martha Poole <marthaepoole@gmail.com>
2014-12-06 16:16:59 -0500

    adding hive code and updated HousingPrices code

--- NYCData/PricesMapper (2).java
+++ NYCData/PricesMapper (2).java
@@ -17,7 +17,6 @@ Double count =1.0;
 
 //make sure that it's a valid 
 if (field.length !=21){
-	String leng = String.valueOf(field.length);
 	context.write(new Text("failed condition0"), new DoubleWritable(count));
 }
 else if (!field[0].contains("1") && !field[0].contains("2") && !field[0].contains("3") && !field[0].contains("4")&& !field[0].contains("5")){
@@ -28,37 +27,32 @@ else if (!field[0].contains("1") && !field[0].contains("2") && !field[0].contain
 else if (field[2].contains("05") || field[2].contains("06") || field[2].contains("21") || field[2].contains("22") || field[2].contains("27") || field[2].contains("29") || field[2].contains("30") || field[2].contains("31") || field[2].contains("32") || field[2].contains("33") || field[2].contains("35") || field[2].contains("36") || field[2].contains("37") || field[2].contains("38") || field[2].contains("39") || field[2].contains("40") || field[2].contains("41") || field[2].contains("43") || field[2].contains("44") || field[2].contains("46") || field[2].contains("47")){
 	context.write(new Text("failed condition2"), new DoubleWritable(count));
 }
-//make sure square feet is listed
-else if(field[15] == "0"){
+//get rid of properties that don't list number of units
+else if (field[13].equals("0")){
 	context.write(new Text("failed condition3"), new DoubleWritable(count));
 }
 //make sure price is listed
 else if (field[19].contains("$-") || field[19].contains("$0")){
 	context.write(new Text("failed condition4"), new DoubleWritable(count));
 }
-else if (field[10] ==""){
+else if (field[10].equals("")){
 	context.write(new Text("failed condition5"), new DoubleWritable(count));
 }
-else if (field[19]==""){
+else if (field[19].equals("")){
 	context.write(new Text("failed condition6"), new DoubleWritable(count));
 }
 else{
-	String sqFeet= field[15];
 	String price =field[19];
-	sqFeet =sqFeet.replaceAll("[$,]", "");
 	price =price.replaceAll("[$,]", "");
-	Double sqFeetInt=Double.parseDouble(sqFeet);
-	Double priceInt=Double.parseDouble(price);
-	if (priceInt==0.0){
+	Double priceDouble=Double.parseDouble(price);
+	String units =field[13];
+	units =units.replaceAll("[$,]", "");
+	Double unitsDouble = Double.parseDouble(units);
+	if (priceDouble==0.0 || unitsDouble==0.0){
 		context.write(new Text("failed condition7"), new DoubleWritable(count));
 	}
 	else{
-	Double pricePerFoot=priceInt/sqFeetInt;
-	String date = field[20];
-	String year = date.replaceAll("[^/]*/[^/]*/", "");
-	String yearZip = field[10]+ "|" +year;
-	context.write(new Text(field[10]), new DoubleWritable(priceInt));
-	//context.write(new Text(price), new DoubleWritable(count));
+	context.write(new Text(field[10]), new DoubleWritable(priceDouble/unitsDouble));
 	}
 	
 }
